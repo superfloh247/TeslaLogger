72c33245630aa81dc3f2580324c0ee0228213a4d
Flo <lindner@Maxi.fritz.box>
2020-10-29 14:04:57 +0100

    initial commit

---
+++ MockServer/APIServer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+namespace MockServer
+{
+    internal class APIServer
+    {
+
+        private HttpListener listener = null;
+
+        public APIServer()
+        {
+            try
+            {
+                listener = new HttpListener();
+                listener.Prefixes.Add("http://*:24780/");
+                listener.Start();
+                Program.Log($"HttpListener bound to http://*:http://*:24780//");
+            }
+            catch (Exception ex)
+            {
+                Program.Log("Exception", ex);
+            }
+            while (listener != null)
+            {
+                try
+                {
+                    ThreadPool.QueueUserWorkItem(OnContext, listener.GetContext());
+                }
+                catch (Exception ex)
+                {
+                    Program.Log("Exception", ex);
+                }
+            }
+        }
+
+        private void OnContext(object o)
+        {
+            try
+            {
+                HttpListenerContext context = o as HttpListenerContext;
+
+                HttpListenerRequest request = context.Request;
+                HttpListenerResponse response = context.Response;
+                switch (true)
+                {
+                    default:
+                        Program.Log("Request: " + request.Url.LocalPath);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Program.Log("Exception", ex);
+            }
+        }
+    }
+}
---
+++ MockServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace MockServer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log("Starting");
+            try
+            {
+                Thread APIServerThread = new Thread(() =>
+                {
+                    APIServer apiServer = new APIServer();
+                })
+                {
+                    Name = "APIServerThread"
+                };
+                APIServerThread.Start();
+            }
+            catch (Exception ex)
+            {
+                Log("Exception", ex);
+            }
+        }
+
+        internal static void Log(string text, Exception ex = null, [CallerFilePath] string _cfp = null, [CallerLineNumber] int _cln = 0)
+        {
+            Console.WriteLine($"DEBUG : {text} ({Path.GetFileName(_cfp)}:{_cln})");
+        }
+
+    }
+}
