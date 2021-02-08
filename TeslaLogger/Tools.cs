9389e6fa7b2a0e8cac92d44c1ce1fbf6be5939c0
Flo <superfloh@gmx.net>
2021-02-08 13:55:35 +0100

    prepare merge

--- TeslaLogger/Tools.cs
+++ TeslaLogger/Tools.cs
@@ -1534,7 +1534,7 @@ WHERE
 
         internal static int GetMothershipKeepDays()
         {
-            int days = 30; // default
+            int days = 14; // default
             try
             {
                 string filePath = FileManager.GetFilePath(TLFilename.SettingsFilename);
@@ -1551,7 +1551,7 @@ WHERE
 
                     if (days == 0)
                     {
-                        days = 30; // default
+                        days = 14; // default
                     }
                 }
             }
